9df1fd41a59ecf0daeaf8552a6b0149e14897e4f
bharadwaj-o <145960166+bharadwaj-o@users.noreply.github.com>
2023-09-26 11:29:47 +0530

    Update readandwrite.py

--- readandwrite.py
+++ readandwrite.py
@@ -1,11 +1,4 @@
-
-# Creating an output file in writing mode
-output_file = open("Write_into.txt", "w",encoding='utf-8')
-  
-# Opening input file and scanning each line
-# from input file and writing in output file
-with open("Read_from.txt", "r",encoding='utf-8') as scan:
-    output_file.write(scan.read())
-  
-# Closing the output file
-output_file.close()
+o = open("Write_into.txt", "w",encoding='utf-8')
+i = open("Read_from.txt", "r",encoding='utf-8')
+o.write(i.read())
+o.close()
